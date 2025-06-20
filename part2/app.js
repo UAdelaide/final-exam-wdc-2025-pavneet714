c2eebd780c49e02f63a38b28e79b0f3ce7fe0ee9
pavmondair <unknown@student.adelaide.edu.au>
2025-06-20 15:12:31 +0000

    [automatic commit] 2025-06-20 15:12:31 UTC 123.243.194.169

--- part2/app.js
+++ part2/app.js
@@ -20,12 +20,12 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('./routes/dogRoutes');
+const dogRoutes = require('./routes/dogRoutes');//ques 17
 
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/dogs', dogRoutes);
+app.use('/api/dogs', dogRoutes);//ques 17
 
 
 
