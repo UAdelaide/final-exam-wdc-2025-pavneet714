851036d35bac84c565c876431cdce9568b696494
pavmondair <unknown@student.adelaide.edu.au>
2025-06-20 08:23:48 +0000

    [automatic commit] 2025-06-20 08:23:48 UTC 113.197.13.145

--- part2/app.js
+++ part2/app.js
@@ -18,23 +18,7 @@ app.use('/api/users', userRoutes);
 
 
 
-// POST /api/login
-app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const [users] = await db.execute('SELECT * FROM Users WHERE username = ? AND password_hash = ?', [username, password]);
-    const user = users[0];
 
-    if (user) {
-      req.session.user = { id: user.user_id, username: user.username, role: user.role };
-      res.json({ role: user.role });
-    } else {
-      res.status(401).json({ error: 'Invalid credentials' });
-    }
-  } catch (err) {
-    res.status(500).json({ error: 'Server error' });
-  }
-});
 
 // Export the app instead of listening here
 module.exports = app;
