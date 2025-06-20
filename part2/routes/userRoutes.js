b2249c7e07742c207162a8a4e1b5cb84ef27ba04
pavmondair <unknown@student.adelaide.edu.au>
2025-06-20 14:21:33 +0000

    [automatic commit] 2025-06-20 14:21:33 UTC 123.243.194.169

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -85,7 +85,7 @@ router.get('/dogs', async (req, res) => {
     `);
     res.json(rows);
   } catch (err) {
-    
+     console.error(err);
     res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
