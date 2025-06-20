1745f9af14c7cfbad7da63bd216158993f5f8d3e
pavmondair <unknown@student.adelaide.edu.au>
2025-06-20 13:04:31 +0000

    [automatic commit] 2025-06-20 13:04:31 UTC 203.217.92.231

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
+
+
 // GET all walk requests (for walkers to view)
 router.get('/', async (req, res) => {
   try {
